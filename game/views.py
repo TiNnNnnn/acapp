39f9214253407007dc5e8fb78fd7c707703bb0bb
Ornamrr <18827906801@139.com>
2022-08-03 14:06:35 +0800

    write my first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(resquest):
+    return HttpResponse("我的第一个网页!!!");
+
+def play(resquest):
+    line1 = '<h1 style="text-align: center">游戏界面"<h1>'
+    return HttpResponse(line1)
