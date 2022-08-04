84eb6a054bb3872414fd5328088163f3f93db32b
Ornamrr <18827906801@139.com>
2022-08-04 20:47:26 +0800

    create project structure

--- game/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def index(resquest):
-    return HttpResponse("我的第一个网页!!!");
-
-def play(resquest):
-    line1 = '<h1 style="text-align: center">游戏界面"<h1>'
-    return HttpResponse(line1)
